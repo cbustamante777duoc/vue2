064d272ddb39d7bba2255bb05186d1330bedf3e5
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2020-06-01 11:23:48 -0400

    agregando evento click y uso de computed

--- js/v-for.js
+++ js/v-for.js
@@ -7,6 +7,9 @@ const app = new Vue({
             {nombre:'pera',cantidad:0},
             {nombre:'kiwi',cantidad:20}
         ],
+        nuevaFruta:'',
+        total: 0 ,
+        nuevaIntegrante:'',
 
         Blackpink:[
             {nombre:'Lalisa',edad:22},
@@ -14,8 +17,45 @@ const app = new Vue({
             {nombre:'Jisoo',edad:25},
             {nombre:'Rose',edad:22}
         ]
+    },
+    methods: {
+        //metodo que agrega una nueva fruta
+        AgregarFruta() {
+            this.frutas.push({
+                nombre: this.nuevaFruta, cantidad:0
+            });
+            //esto deja en blanco el input
+            this.nuevaFruta='';
+        },
+        AgregarNuevaIntegrante(){
+            this.Blackpink.push({
+                nombre: this.nuevaIntegrante, edad:22
+            });
+            this.nuevaIntegrante='';
 
+        }
+    },
+    computed: {
+        sumarFrutas() {
 
+           this.total = 0; 
+
+           for(item of this.frutas) {
+
+               this.total = this.total + item.cantidad;
+           }
+           // retorna la cantidad sumada
+           return this.total;
+        },
+        /*
+        sumarEdades() {
+            this.total = 0;
+            for(item of this.Blackpink){
+                this.total = this.total + item.edad;
+            }
+            return this.total;
+
+        }*/
     }
 
 });
