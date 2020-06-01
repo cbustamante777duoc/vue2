f79fafbece67507fc8aab8ec52f6c959bab879cf
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2020-05-31 21:27:03 -0400

    uso de v-for y  v-if

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vue2
 curso de vue
+
+# v-for
+uso de recorrido de elementos con v-for y uso de v-if(condicionales)
---
+++ js/v-for.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+    el:'#app',
+    data: {
+        titulo:'hola mundo',
+        frutas:[
+            {nombre:'manzana',cantidad:20},
+            {nombre:'pera',cantidad:0},
+            {nombre:'kiwi',cantidad:20}
+        ],
+
+        Blackpink:[
+            {nombre:'Lalisa',edad:22},
+            {nombre:'Jennie',edad:23},
+            {nombre:'Jisoo',edad:25},
+            {nombre:'Rose',edad:22}
+        ]
+
+
+    }
+
+});
